ae4219f331fdc2622a440ba29e071a17df741259
leyosu23 <leyosu23@gmail.com>
2021-12-11 13:25:25 -0500

    JavaScript Expressions in JSX & ES6 Template Literals

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
 
-var React = require("react");
-var ReactDOM = require("react-dom");
+const fname = "YS";
+const lname = "Willson"
 
 ReactDOM.render(
-<div>
-  <h1>H1 heading</h1>
-  <ul>
-  <li>hey</li>
-  <li>hey</li>
-  <li>hey</li>
-  </ul>
-</div>, 
-  document.getElementById("root"));
-
+  <div>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>your lucky numbers is {Math.floor(Math.random() * 10 + 1)}
+    </p>
+  </div>,
+  document.getElementById("root")
+);
